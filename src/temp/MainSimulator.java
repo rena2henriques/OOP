ce9957b5b764be98e7638160beb27d433fdc311a
rena2henrique <renato.manao@gmail.com>
2018-04-25 15:21:46 +0100

    initiated code project

---
+++ src/temp/MainSimulator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package temp;
+
+/**
+ * @author renato
+ *
+ */
+public class MainSimulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

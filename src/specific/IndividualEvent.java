2a364816026858d3df6f61901a616b98d1a16f08
mariana <mariana.galrinho@tecnico.ulisboa.pt>
2018-05-01 16:28:03 +0100

    added functions to IndividualEvent

--- src/specific/IndividualEvent.java
+++ src/specific/IndividualEvent.java
@@ -4,11 +4,18 @@ import general.Event;
 
 public abstract class IndividualEvent extends Event{
 	
-	Individual individual;
+	private Individual individual;
 	
 	IndividualEvent(double time,Individual individual){
 		super(time);
 		this.individual = individual;
 	}
 	
+	public Individual getIndividual() {
+		return individual;
+	}
+	
+	public void setIndividual(Individual individual) {
+		this.individual=individual;
+	}
 }

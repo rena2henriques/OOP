6fdd2a6d1adbb16a7a226ef7d6184ab5a08facb5
rena2henrique <renato.manao@gmail.com>
2018-05-10 10:37:01 +0100

    create and wrote package-info files for all packages

---
+++ src/main/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package includes a class with a main method just for running the simulation
+ * 
+ * @author Group 6 - 81588, 81607 and 81669
+ * Course of Objects Oriented Language
+ * 
+ */
+package main;
--- src/specific/package-info.java
+++ src/specific/package-info.java
@@ -1,8 +1,11 @@
 /**
- * 
  * This package includes all the classes that are specific to this project
- * TODO
+ * <p> The classes from this package are very specific for the application we are doing and shouldn't be changed in order
+ * to keep the well being of the program. If new features are needed then new classes should be created that inherits classes
+ * from this package
  * 
- *
+ * @author Group 6 - 81588, 81607 and 81669
+ * Course of Objects Oriented Language
+ * 
  */
 package specific;

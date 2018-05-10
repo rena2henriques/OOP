2547188f6d8989034ffbe451c88346c77a566623
rena2henrique <renato.manao@gmail.com>
2018-05-10 10:37:20 +0100

    added some more comments

--- src/specific/package-info.java
+++ src/specific/package-info.java
@@ -1,8 +1,9 @@
 /**
- * This package includes all the classes that are specific to this project
+ * This package includes all the classes that are specific to this project 
  * <p> The classes from this package are very specific for the application we are doing and shouldn't be changed in order
  * to keep the well being of the program. If new features are needed then new classes should be created that inherits classes
- * from this package
+ * from this package. This simulation consists in individuals moving around a map and doing other events like death or reproduction.
+ * In the end of the simulation the path of the best individual is printed.
  * 
  * @author Group 6 - 81588, 81607 and 81669
  * Course of Objects Oriented Language
